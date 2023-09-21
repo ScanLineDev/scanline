e2e4cfe321070e701a0570f0d01aafdbff6f7300
scottfits <iscottonline@gmail.com>
2023-09-20 19:08:52 -0700

    cleanup

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 #! /bin/bash
+echo "👋🏼 Welcome to AI CLI, glad you're here!"
 curl -O https://raw.githubusercontent.com/stephenkfrey/ailinter/reviewme/dist/aicli
+echo "🔐 We need your password to install the CLI in the right folder."
 chmod +x aicli
 sudo mv aicli /usr/local/bin/ai
+echo "✅ All done here. Run 'ai' to get started. 🚀"
