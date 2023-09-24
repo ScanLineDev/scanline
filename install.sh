7f03a5c93255db665fd498c24297252437148d7d
scottfits <iscottonline@gmail.com>
2023-09-23 21:54:14 -0700

    progress bar

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 echo "👋🏼 Welcome to Scanline, so glad you're here!"
-curl -Lso aicli https://github.com/ScanLineDev/scanline/releases/download/v0.1/aicli
+curl -L --progress-bar -o aicli https://github.com/ScanLineDev/scanline/releases/download/v0.1/aicli
 chmod +x aicli
 echo "🔐 We need your password to install the CLI in the right folder."
 read -s SUDO_PASSWORD
