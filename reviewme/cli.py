5f56bb637f46f4eb2c9787d7b8e0e172aa6a10ab
Kyle Morris <kyle@banana.dev>
2023-10-01 22:41:21 -0700

    Add click-default-group to requirements.txt and refactor CLI code

--- reviewme/cli.py
+++ reviewme/cli.py
@@ -2,18 +2,16 @@
 from reviewme.ailinter import ailinter 
 import click
 import logging
+from click_default_group import DefaultGroup
 
 logging.basicConfig(level=logging.WARN)
 
-@click.group(invoke_without_command=True)
-@click.pass_context
-def cli(ctx):
-    if ctx.invoked_subcommand is None:
-        print ("👨🏻‍💻 Starting AI code review on branch")
-        ailinter.run("branch", "")
-        print ("✅ Code review complete.")
+@click.group(cls=DefaultGroup, default='run', default_if_no_args=True)
+def cli():
+    print("group execution")
+    pass
 
-@click.command()
+@cli.command()
 @click.option('--scope', default="branch", help='Scope of code review. Can be "commit", "branch", or "repo". Defaults to "branch"')
 @click.option('--file', default="", help='Select a specific file to review. Defaults to all files in scope.')
 @click.option('--model', default="gpt-4", help='Specify openai model to use listed @ https://platform.openai.com/docs/models/overview. For example gpt-4-32k supports 4x larger files whereas gpt-3.5-turbo should be faster and >10x cheaper. Defaults to gpt-4.')
@@ -25,8 +23,6 @@ def run(scope, file, model):
 
     ailinter.run(scope, file, model)
     print ("✅ Code review complete.")
-
-cli.add_command(run)
 
 if __name__ == '__main__':
     cli()
