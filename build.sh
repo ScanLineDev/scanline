a02e8c055289fada3d630dde0b612663ab224458
stephenkfrey <2404105+stephenkfrey@users.noreply.github.com>
2023-10-05 22:13:55 -0700

    Remove unneeded modules

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 pyinstaller scanline-build.spec
 echo "🔐 We need your password to install the CLI in the right folder. (This is only used locally, we never see this.)"
+
 sudo mv dist/scanline /usr/local/bin/scanline
