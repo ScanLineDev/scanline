cb074bbb4e8af13f83968765412f311d0f7cd1d5
Kyle Morris <kyle@banana.dev>
2023-10-09 23:21:24 -0700

    Add check for current branch and display error message if not in a git repo

--- reviewme/cli.py
+++ reviewme/cli.py
@@ -17,6 +17,10 @@ def cli():
 @click.option('--model', default="gpt-4", help='Specify openai model to use listed @ https://platform.openai.com/docs/models/overview. For example gpt-4-32k supports 4x larger files whereas gpt-3.5-turbo should be faster and >10x cheaper. Defaults to gpt-4.')
 def run(scope, file, model):
     branch = ailinter.get_current_branch()
+    if branch == None:
+        print ("👨🏻‍💻 You are not in a git repo. Please run this command in a git repo.")
+        return
+
     if scope == "branch" and (branch == "main" or branch == "master"):
         print ("👨🏻‍💻 You are on the {0} branch already. Please checkout a different branch to see a review on the branch differences. Or, if you want to compare local changes on your main branch to itself then run \"scanline --scope commit\".".format(branch))
         return
